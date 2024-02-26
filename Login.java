cf31228e641998d21447e5310086d51a6eba53b5
MohdShahbazKhan <mohdshahbaz.nahidkhan@gmail.com>
2024-02-26 22:49:16 +0530

    bugs fixes

--- Login.java
+++ Login.java
@@ -1,7 +1,10 @@
 class Login
 {
 public static void main(String[]args)
-
+System.out.println("hello ")
+System.out.println("hello ")
+System.out.println("hello ")
+System.out.println("hello ")
 
 
 }
