2bcb2f937d263bd2452633d22b3dcd75c1e604a8
MohdShahbazKhan <mohdshahbaz.nahidkhan@gmail.com>
2024-02-26 22:22:31 +0530

    this is my first commit

---
+++ A.java
@@ -0,0 +1,10 @@
+class A{
+
+public static void main (String []args)
+
+System.out.println("hello i am Class A")
+
+
+}
+
+
---
+++ B.java
@@ -0,0 +1,8 @@
+class B{
+
+public static void main (String []args)
+
+System.out.println("hello i am Class B")
+
+
+}
