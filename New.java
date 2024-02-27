1aa7d6ccd2cd54ce20c2611a5a8454d1544a8973
MohdShahbazKhan <mohdshahbaz.nahidkhan@gmail.com>
2024-02-27 12:56:33 +0530

    this is my new class

---
+++ New.java
@@ -0,0 +1,9 @@
+class New
+{
+
+public static void main(String [] args)
+
+Sysout.out.println("this is my new class")
+
+
+}
---
+++ New2.java
@@ -0,0 +1,9 @@
+class New2
+{
+
+public static void main(String [] args)
+
+Sysout.out.println("this is my new2 class")
+
+
+}
